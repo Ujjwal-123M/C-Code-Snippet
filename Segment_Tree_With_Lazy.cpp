0399d3130f004b2d19d7c532a42a10bd9405197d
Ujjwal Mishra <111675075+Ujjwal-123M@users.noreply.github.com>
2024-03-05 12:16:21 +0530

    Update Segment_Tree_With_Lazy.cpp

--- Segment_Tree_With_Lazy.cpp
+++ Segment_Tree_With_Lazy.cpp
@@ -1,6 +1,8 @@
-#include<bits/stdc++.h>
+// Range update query cses 
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long 
+#define int long long 
 #define N 200005
 ll arr[N]; // 1 based indexing me array me insert karni hai
 ll lazy[4*N];
@@ -15,7 +17,7 @@ void buildTree(int pos,int l,int r){
     buildTree(2*pos+1, mid+1, r);
     seg[pos] = seg[2*pos]+ seg[2*pos+1];
 }
-void update(int l,int r,int pos,int ql,int qr,int val){
+void update(int l,int r,int pos,int ql,int qr,ll val){
       if(lazy[pos]!=0){
         int dx=lazy[pos];
         lazy[pos]=0;
@@ -24,7 +26,7 @@ void update(int l,int r,int pos,int ql,int qr,int val){
         lazy[2*pos]+=dx,lazy[2*pos+1]+=dx;
     }
      
-    if(l>ql || r<qr) return;
+    if(l>r || l>qr || r<ql) return;
     if(l>=ql && r<=qr){
       int dx=(r-l+1)*val;
       seg[pos]+=dx;
@@ -47,19 +49,33 @@ ll getsum(int l,int r,int ql,int qr,int pos){
         lazy[2*pos]+=dx,lazy[2*pos+1]+=dx;
     }
 
-    if(ql>r || qr<l) return 0;
+    if(l>r || l>qr || r<ql) return 0;
     if(ql<=l && qr>=r) return seg[pos];
     ll mid=(l+r)/2;
     return getsum(l,mid,ql,qr,2*pos) + getsum(mid+1,r,ql,qr,2*pos+1);
 }
 
-int main()
+signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
-     
-      memset(lazy,0,sizeof(lazy));
-
+    int n,q;
+    cin>>n>>q;
+    for(int i=1;i<=n;i++) cin>>arr[i];;
+    buildTree(1,1,n);
+    memset(lazy,0,sizeof(lazy));
+    while(q--){
+     int type;
+     cin>>type;
+     if(type==2) {
+        int k;cin>>k;
+        cout<<getsum(1,n,k,k,1)<<"\n";
+     }
+     else {
+        ll a,b,u;
+        cin>>a>>b>>u;
+        update(1,n,1,a,b,u);
+     }
+    }
     return 0;
 }
